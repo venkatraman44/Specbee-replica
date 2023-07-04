b69aa5c28be9e98d901b1131e5d2aaa01732ccaa
venkatraman <kmlvenkat@gmail.com>
2023-07-04 23:33:04 +0530

    ghfh

---
+++ js/navbutton.js
@@ -0,0 +1,5 @@
+function navbutton(element) {
+    var navbuttonMenu = document.getElementById("navbuttonMenu");
+    navbuttonMenu.classList.toggle("menu");
+    element.classList.toggle("open");
+}
